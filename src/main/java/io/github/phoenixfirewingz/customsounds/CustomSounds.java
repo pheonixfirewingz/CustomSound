d96c85fe425a61af7aea0b5c8d9a12099826eeca
Phoenixfirewingz <lukethemodded@gmail.com>
2023-03-01 14:38:16 +0000

    add accesswidener

--- src/main/java/io/github/phoenixfirewingz/customsounds/CustomSounds.java
+++ src/main/java/io/github/phoenixfirewingz/customsounds/CustomSounds.java
@@ -1,35 +1,34 @@
 package io.github.phoenixfirewingz.customsounds;
 
-
 import com.mojang.datafixers.types.Type;
-import io.github.phoenixfirewingz.customsounds.block.Blocks;
+import io.github.phoenixfirewingz.customsounds.block.SoundNode;
 import io.github.phoenixfirewingz.customsounds.block.entity.SoundNodeEntity;
 import io.github.phoenixfirewingz.customsounds.client.screen.NodeScreenHandle;
-import io.github.phoenixfirewingz.customsounds.item.Items;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.datafixer.TypeReferences;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 
-
 public class CustomSounds implements ModInitializer {
     public static final String MOD_ID = "custom_sounds";
+    public static Block SOUND_NODE;
     public static ScreenHandlerType<NodeScreenHandle> SOUND_NODE_SCREEN_HANDLER_TYPE;
-    public final static BlockEntityType<SoundNodeEntity> SOUND_NODE_ENTITY_BLOCK_ENTITY_TYPE;
-    private Items items = new Items();
-
-    static {
-        SOUND_NODE_ENTITY_BLOCK_ENTITY_TYPE = create("sound_node",BlockEntityType.Builder.create(SoundNodeEntity::new, Blocks.SOUND_NODE));
-        SOUND_NODE_SCREEN_HANDLER_TYPE = new io.github.phoenixfirewingz.customsounds.mixin.ScreenHandlerType<NodeScreenHandle>().patch_register("sound_node",NodeScreenHandle::new);
-    }
+    public static BlockEntityType<SoundNodeEntity> SOUND_NODE_ENTITY_BLOCK_ENTITY_TYPE;
 
     @Override
     public void onInitialize() {
+        SOUND_NODE = Registry.register(Registries.BLOCK, CustomSounds.genID("sound_node"), new SoundNode(AbstractBlock.Settings.of(Material.BARRIER).noCollision().dropsNothing().nonOpaque().noBlockBreakParticles().strength(-1.0F, 3600000.8F)));
+        SOUND_NODE_ENTITY_BLOCK_ENTITY_TYPE = create("sound_node",BlockEntityType.Builder.create(SoundNodeEntity::new, SOUND_NODE));
+        SOUND_NODE_SCREEN_HANDLER_TYPE = register("sound_node",NodeScreenHandle::new);
     }
 
     public static Identifier genID(String name)
@@ -40,5 +39,9 @@ public class CustomSounds implements ModInitializer {
     private static <T extends BlockEntity> BlockEntityType<T> create(String id, BlockEntityType.Builder<T> builder) {
         Type<?> type = Util.getChoiceType(TypeReferences.BLOCK_ENTITY, id);
         return Registry.register(Registries.BLOCK_ENTITY_TYPE, id, builder.build(type));
+    }
+
+    private static <T extends ScreenHandler> ScreenHandlerType register(String id, ScreenHandlerType.Factory<T> factory) {
+        return Registry.register(Registries.SCREEN_HANDLER, id, new ScreenHandlerType<T>(factory));
     }
 }
